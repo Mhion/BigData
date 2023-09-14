441ab9ccb955e0e73e0dd32d144836870a5beb8a
wishinefall <wishinefall@naver.com>
2023-09-14 11:00:05 +0900

    week02

--- week02_numpy.py
+++ week02_numpy.py
@@ -1,6 +1,36 @@
 import numpy as np
+import random
+import tkinter as tk  # Built in GUI
 
-v = np.array([1, 3, -9, 2])
-# v = np.numpy([1, 3, -9, 2]. )
+def click_button():
+    n = int(en_number.get())
+    l = [random.randint(1, 100) for i in range(n)]
+    v = np.array(l, dtype='int16')
+    lbl_result.config(text=v)
 
+
+window = tk.Tk()
+window.title('numpy gui version v0.7')
+window.geometry('300x150')
+
+# create widget
+lbl_result = tk.Label(text="random numpy array")
+en_number = tk.Entry()
+btn_click = tk.Button(text="click me!", command=click_button)
+
+# widget layout
+lbl_result.pack()
+en_number.pack(fill='x')
+btn_click.pack(fill='x')
+
+window.mainloop()
+# n = int(input("input number : "))
+# l = [random.randint(1, 100) for i in range(n)]
+# v = np.array(l, dtype='int16')
+# print(v)
+
+n = int(input("input number : "))
+l = [random.randint(1, 100) for i in range(n)]
+v = np.array(l, dtype='int16')
 print(v)
+# print(v.ndim, v.shape, v.data, v.dtype, v.strides)
